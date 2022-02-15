149e3e5e9ad8303b96cd2e1d42dcaa7cce8b047d
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-02-15 20:08:30 +0300

    refactor: rename functions and variables, add support for yaml files

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -2,35 +2,74 @@
 
 namespace Differ\Differ;
 
+use function Differ\Parsers\parse;
+
 function getFileFullPath(string $pathToFile): string
 {
     return str_starts_with($pathToFile, '/') ? $pathToFile : __DIR__ . '/../' . $pathToFile;
 }
 
+function getFileExtension($pathToFile)
+{
+    if (str_ends_with($pathToFile, '.json')) {
+        return 'json';
+    } elseif (str_ends_with($pathToFile, '.yml') || str_ends_with($pathToFile, 'yaml')) {
+        return 'yaml';
+    }
+
+    return null;
+}
+
+function parseFile($pathToFile)
+{
+    $filePath = getFileFullPath($pathToFile);
+    $fileContent = file_get_contents($filePath);
+    $fileExtension = getFileExtension($pathToFile);
+
+    return parse($fileContent, $fileExtension);
+}
+
+function stringify($value)
+{
+    $valueType = gettype($value);
+    switch ($valueType) {
+        case 'boolean':
+            return $value ? 'true' : 'false';
+        case 'NULL':
+            return 'null';
+        default:
+            return (string) $value;
+    }
+}
+
+
 function genDiff(string $pathToFile1, string $pathToFile2): string
 {
-    $fileContent1 = file_get_contents(getFileFullPath($pathToFile1));
-    $fileContent2 = file_get_contents(getFileFullPath($pathToFile2));
-    $jsonContent1 = json_decode($fileContent1, true);
-    $jsonContent2 = json_decode($fileContent2, true);
-    $allJsonsKeys = array_unique(array_merge(array_keys($jsonContent1), array_keys($jsonContent2)));
-    sort($allJsonsKeys);
+    $dataBefore = parseFile($pathToFile1);
+    $dataAfter = parseFile($pathToFile2);
+    $dataKeys = array_unique(
+        array_merge(
+            array_keys($dataBefore),
+            array_keys($dataAfter)
+        )
+    );
+    sort($dataKeys);
 
     $result = ['{'];
-    foreach ($allJsonsKeys as $key) {
-        $val1 = json_encode($jsonContent1[$key] ?? null);
-        $val2 = json_encode($jsonContent2[$key] ?? null);
-        if (array_key_exists($key, $jsonContent1) && array_key_exists($key, $jsonContent2)) {
-            if ($val1 === $val2) {
-                $result[] = "    {$key}: {$val1}";
+    foreach ($dataKeys as $key) {
+        $valBefore = stringify($dataBefore[$key] ?? null);
+        $valAfter = stringify($dataAfter[$key] ?? null);
+        if (array_key_exists($key, $dataBefore) && array_key_exists($key, $dataAfter)) {
+            if ($valBefore === $valAfter) {
+                $result[] = "    {$key}: {$valBefore}";
             } else {
-                $result[] = "  - {$key}: {$val1}";
-                $result[] = "  + {$key}: {$val2}";
+                $result[] = "  - {$key}: {$valBefore}";
+                $result[] = "  + {$key}: {$valAfter}";
             }
-        } elseif (array_key_exists($key, $jsonContent1)) {
-            $result[] = "  - {$key}: {$val1}";
+        } elseif (array_key_exists($key, $dataBefore)) {
+            $result[] = "  - {$key}: {$valBefore}";
         } else {
-            $result[] = "  + {$key}: {$val2}";
+            $result[] = "  + {$key}: {$valAfter}";
         }
     }
     $result[] = '}';
