0487101f4fa399893fb0bf703870696086f74d38
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-02-22 21:02:33 +0300

    refactor: change function names

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -19,31 +19,31 @@ function toString($value)
     }
 }
 
-function stringify($data, $depth)
+function stringifyArray($data, $depth)
 {
     $result = ['{'];
     foreach ($data as $key => $value) {
         if (is_array($value)) {
-            $value = stringify($value, $depth + 1);
+            $value = stringifyArray($value, $depth + 1);
         }
-        $result[] = getSameRow($key, $value, $depth);
+        $result[] = getIntendedRow($key, $value, $depth);
     }
     $result[] = str_repeat(INDENT, $depth - 1) . "}";
 
     return implode(PHP_EOL, $result);
 }
 
-function getAddRow($key, $value, $depth = 1)
+function getAddedRow($key, $value, $depth = 1)
 {
     return str_repeat(INDENT, $depth - 1) . ADD_ROW . "{$key}: {$value}";
 }
 
-function getRemoveRow($key, $value, $depth = 1)
+function getRemovedRow($key, $value, $depth = 1)
 {
     return str_repeat(INDENT, $depth - 1) . REMOVE_ROW . "{$key}: {$value}";
 }
 
-function getSameRow($key, $value, $depth = 1)
+function getIntendedRow($key, $value, $depth = 1)
 {
     return str_repeat(INDENT, $depth) . "{$key}: {$value}";
 }
@@ -56,27 +56,27 @@ function iter(array $data, $depth = 1)
             [$valBefore, $valAfter] = $value;
             if (!is_null($valBefore)) {
                 $valBefore = is_array(json_decode($valBefore, true))
-                    ? stringify(json_decode($valBefore, true), $depth + 1)
+                    ? stringifyArray(json_decode($valBefore, true), $depth + 1)
                     : toString(json_decode($valBefore, true));
             }
             if (!is_null($valAfter)) {
                 $valAfter = is_array(json_decode($valAfter, true))
-                    ? stringify(json_decode($valAfter, true), $depth + 1)
+                    ? stringifyArray(json_decode($valAfter, true), $depth + 1)
                     : toString(json_decode($valAfter, true));
             }
             if ($valBefore === $valAfter) {
-                $result[] = getSameRow($key, $valBefore, $depth);
+                $result[] = getIntendedRow($key, $valBefore, $depth);
             } elseif (is_null($valBefore)) {
-                $result[] = getAddRow($key, $valAfter, $depth);
+                $result[] = getAddedRow($key, $valAfter, $depth);
             } elseif (is_null($valAfter)) {
-                $result[] = getRemoveRow($key, $valBefore, $depth);
+                $result[] = getRemovedRow($key, $valBefore, $depth);
             } else {
-                $result[] = getRemoveRow($key, $valBefore, $depth);
-                $result[] = getAddRow($key, $valAfter, $depth);
+                $result[] = getRemovedRow($key, $valBefore, $depth);
+                $result[] = getAddedRow($key, $valAfter, $depth);
             }
         } else {
             $nestedFormat = iter($value, $depth + 1);
-            $result[] = str_repeat(INDENT, $depth) . "{$key}: {";
+            $result[] = getIntendedRow($key, '{', $depth);
             foreach ($nestedFormat as $str) {
                 $result[] = $str;
             }
